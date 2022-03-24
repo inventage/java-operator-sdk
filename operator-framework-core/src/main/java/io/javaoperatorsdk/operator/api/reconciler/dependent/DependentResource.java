72d525cc0e4d0d5a1bf7839101b5aa14e4130028
csviri <csviri@gmail.com>
2022-03-24 14:33:02 +0100

    docs: additional features, reformat

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/dependent/DependentResource.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/dependent/DependentResource.java
@@ -6,7 +6,20 @@ import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 
 public interface DependentResource<R, P extends HasMetadata> {
+
+  /**
+   * @param primary associated primary resource
+   * @param context of the reconciliation
+   * @return result of the reconciliation
+   */
   ReconcileResult<R> reconcile(P primary, Context<P> context);
 
+  /**
+   * The intention with get resource is to return the actual state of the resource. Usually from a local cache,
+   * what was updated after the reconciliation, or typically from the event source that caches the resources.
+   *
+   * @param primaryResource associated
+   * @return the reconciled resource
+   */
   Optional<R> getResource(P primaryResource);
 }
